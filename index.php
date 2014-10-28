a8300c076a0205771e381dd98cf915a1868b0a18
ishant-kumar <ishant.kumar@intelligrape.com>
2014-10-28 14:04:14 +0530

    index php file modified

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "The time is ..." . date("h:i:sa");
-echo "<br>";
+echo "<br><hr>";
 $cars=array("Volvo","BMW","Toyota");
 echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
 ?> 
